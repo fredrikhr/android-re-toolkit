95da657c735df242104643fdf699adacd26bfa93
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-01-05 14:12:43 +0100

    Add initial .NET solution, project and support files

---
+++ src/Couven92.AndroidRe.ToolkitConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Couven92.AndroidRe.ToolkitConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
